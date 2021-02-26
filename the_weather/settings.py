adccb43d829f969ffddc0c7683738cf6b287b0c0
Pinky Mononyane <64901994+Pinky-alt-spec@users.noreply.github.com>
2021-02-26 06:14:35 -0800

    Update settings.py

--- the_weather/settings.py
+++ the_weather/settings.py
@@ -120,3 +120,8 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/2.0/howto/static-files/
 
 STATIC_URL = '/static/'
+
+if os.getcwd() == '/app':
+    SECURE_PROXY_SSL_HEADER = ('HTTP_X_FORWARDED_PROTO', 'https')
+    SECURE_SSL_REDIRECT = True
+    DEBUG = False
